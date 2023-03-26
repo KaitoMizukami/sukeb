937befc1736af6fff11454e7f2a25f0b31d0cde5
KaitoMizukami <kaito.program@gmail.com>
2023-03-27 04:38:40 +0900

    Create creation and change form for user model

---
+++ authentications/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
+class UserCreationForm(forms.ModelForm):
+    password = forms.CharField(label='パスワード', min_length=8, widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label='確認用パスワード', widget=forms.PasswordInput())
+    username = forms.CharField(label='ユーザーネーム')
+    email = forms.EmailField(label='メールアドレス')
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'password')
+    
+    def clean(self):
+        """ 
+        フォームを検証する
+        """
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise ValidationError('パスワードが一致しません')
+    
+    def save(self, commit=False):
+        """ 
+        ユーザーフォームを保存してユーザーを作成する
+        """
+        user = super().save(commit=False)
+        password = self.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'password', 'is_staff', 'is_superuser', 'is_active')
+
+    def clean_password(self):
+        return self.initial['password']
