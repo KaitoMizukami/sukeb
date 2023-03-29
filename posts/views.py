fb0393427ad8af2808f58a8abe31ccb0934800f7
KaitoMizukami <kaito.program@gmail.com>
2023-03-29 17:11:37 +0900

    Create a PostsListView

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView
+)
+from django.db.models import Q
 
-# Create your views here.
+from .models import Post
+from .prefectures import PREFECTURE_CHOICES
+
+
+class PostsListView(ListView):
+    """
+    全ての投稿データを取得し,対応したHTMLに渡す
+    """
+    template_name = 'posts/posts_list.html'
+    model = Post
+
+    def get_queryset(self, **kwargs):
+        """ 
+        デフォルトでPostモデルの全てのデータをリストで返す
+        queryキーワードがある場合はマッチしたPostモデルのデータをリストで返す
+        """
+        queryset = super().get_queryset(**kwargs)
+        # GETリクエストパラメータにqueryがあれば、それでフィルタする
+        query_keyword = self.request.GET.get('query')
+        if query_keyword:
+            # locationモデルのprefectureとquery_keywordが一致するデータをフィルタする
+            queryset = Post.objects.filter(
+                Q(skatepark__prefecture__contains=query_keyword)
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        """
+        テンプレートに渡すコンテキストにデータを加える
+        """
+        context = super().get_context_data(**kwargs)
+        # 全都道府県のリストをコンテキストに追加
+        context['prefectures'] = PREFECTURE_CHOICES
+        return context
