9650a54248ba3f8458d829f9bd4367467121d2a6
KaitoMizukami <kaito.program@gmail.com>
2023-04-06 01:45:14 +0900

    Add urlConf to PostsDeleteView

--- posts/urls.py
+++ posts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.PostsListView.as_view(), name='list'),
     path('posts/detail/<int:pk>', views.PostsDetailView.as_view(), name='detail'),
     path('posts/create/', views.PostsCreateView.as_view(), name='create'),
+    path('posts/delete/<int:pk>', views.PostsDeleteView.as_view(), name='delete'),
 ]
