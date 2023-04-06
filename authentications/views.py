3a2eb980acab796bfb018aa060eb94b07bdf2911
KaitoMizukami <kaito.program@gmail.com>
2023-04-07 00:02:08 +0900

    Create a AuthenticationsSignupView

--- authentications/views.py
+++ authentications/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import FormView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .forms import UserCreationForm
+
+
+User = get_user_model()
+
+
+class AuthenticationsSignupView(FormView):
+    """ 
+    ユーザーの登録フォームをHTMLに渡す
+    """
+    template_name = 'authentications/authentications_signup.html'
+    form_class = UserCreationForm
+
+    def post(self, request, *args, **kwargs):
+        """ 
+        Postリクエスト時の処理
+        Userモデルのフォームを検証し、データを保存する
+        検証成功すれば投稿一覧ページにリダイレクトし、自動でログインした状態にする
+        検証失敗すれば登録ページを返す
+        """
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            user = authenticate(email=user_form.cleaned_data['email'], password=user_form.cleaned_data['password'])
+            login(request, user)
+            return redirect('posts:list')
+        else:
+            return render(request, self.template_name, {'form': user_form})
